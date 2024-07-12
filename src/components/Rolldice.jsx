0623fb41d3510ec3d09a652e6bff9c2e6f172150
bhagesh01 <bansodebhageshbb08@gmail.com>
2024-07-12 14:07:29 +0530

    yessboyy

--- src/components/Rolldice.jsx
+++ src/components/Rolldice.jsx
@@ -5,7 +5,7 @@ const Rolldice = ({currentDice , rollDice}) => {
   return (
     <div>
       <div className='flex items-center justify-center h-fit mt-10'>
-      <img src={`./../public/images/dice_${currentDice}.png`} alt="dice_1" className='cursor-pointer' onClick={rollDice}/>
+      <img src={`/images/dice_${currentDice}.png`} alt={`dice_${currentDice}`}  className='cursor-pointer' onClick={rollDice}/>
     </div>
       <p className='text-3xl font-semibold'>click on the dice to roll.</p>
     </div>
